ac68ce902d1b92eaacd45511f98e8cf8c4cecc2e
greenjaguarr <marksmit2002@ziggo.nl>
2024-12-19 14:39:25 +0100

    helft van de scores toegevoegd

--- poker_utils.py
+++ poker_utils.py
@@ -11,13 +11,13 @@ class Kaart:
         self.waarde = waarde
 
 
-kaarten1 = [Kaart("harten","K"),
+kaarten1 = [Kaart("harten","9"),
            Kaart("schoppen","K"),
            Kaart("klaveren","K"),
-           Kaart("harten","K"),
-           Kaart("harten","V"),
+           Kaart("harten","B"),
+           Kaart("harten","9"),
            Kaart("harten","T"),
-           Kaart("harten","6")]
+           Kaart("harten","V")]
 
 def calc_waarde(kaarten:list[Kaart]):
     # kaarten is een list met lengte 7
@@ -37,30 +37,62 @@ def calc_waarde(kaarten:list[Kaart]):
             kaartwaardefrequentie[kaart.waarde] +=1
         else:
             kaartwaardefrequentie.update({kaart.waarde:1})
-
-    kaartkleurfrequentie_list = list(kaartkleurfrequentie.values())
-    for i in range(len(kaartkleurfrequentie)):
-        if kaartkleurfrequentie_list[i] >= '5':
-            handresult.append("flush")
-        
+    
     kaartwaardefrequentie_list = list(kaartwaardefrequentie.values())
-    for i in range(len(kaartwaardefrequentie)):
-        if kaartwaardefrequentie_list[i] == '2':
-            handresult.append("pair")
-        if kaartkleurfrequentie_list[i] == '3':
-            handresult.append("3 of a kind")
-        if kaartkleurfrequentie_list[i] == '4':
-            handresult.append("quads")
+    kaartkleurfrequentie_list = list(kaartkleurfrequentie.values())
+    
+    # checking for flush
+    if max(kaartkleurfrequentie_list) >= 5:
+        waardes_in_flush = []
+        
+        for kaart in kaarten:
+            if kaartkleurfrequentie[kaart.kleur] >= 5:
+                waardes_in_flush.append(kaart.waarde)
+        
+        handresult.append(["flush", waardes_in_flush])
+
+    # checking for quads
+    if max(kaartwaardefrequentie_list) == 4:
+        for kaart in kaarten: 
+            if kaartwaardefrequentie[kaart.waarde] == 4:
+                waarde_van_quads = kaart.waarde
+        
+        handresult.append(["quads", waarde_van_quads])
+        kaartwaardefrequentie_list.remove(4)
+
+    # checking for trips
+    for i in range(2):
+        if max(kaartwaardefrequentie_list) == 3:
+            for kaart in kaarten: 
+
+                if kaart.waarde in kaartwaardefrequentie:
+                    if kaartwaardefrequentie[kaart.waarde] == 3:
+                        waarde_van_trips = kaart.waarde
+            
+            handresult.append(["trips", waarde_van_trips])
+
+            kaartwaardefrequentie.pop(waarde_van_trips)
+            kaartwaardefrequentie_list.remove(3)
+    
+    # checking for pairs
+    for i in range(3):
+        if max(kaartwaardefrequentie_list) == 2:
+            for kaart in kaarten: 
+
+                if kaart.waarde in kaartwaardefrequentie:
+                    if kaartwaardefrequentie[kaart.waarde] == 2:
+                        waarde_van_pair = kaart.waarde
+            
+            handresult.append(["pair", waarde_van_pair])
+
+            kaartwaardefrequentie.pop(waarde_van_pair)
+            kaartwaardefrequentie_list.remove(2)
         
 
     print(handresult)
-        
-            
-    
-    
 
 
-    return ...
+    return handresult
 
 def compare(kaarten1, kaarten2):
     '''Return "1" if kaarten1 is beter,
